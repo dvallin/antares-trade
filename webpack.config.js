d44e31e615697c778adfdd5f56022351e1e42f84
Maximilian Schuler <schulermaximilian@gmail.com>
2021-02-09 22:57:31 +0100

    remove husky and public path hussle

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js',
+    publicPath: 'antares-trade',
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
